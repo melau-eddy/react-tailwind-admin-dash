26fb0a4001acfff473eff80ea8ce1ea282bbf2fe
melau-eddy <eddymutugik@gmail.com>
2025-03-11 04:11:16 +0300

    updated components file in src

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+ // Route,
+  //Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home/>
+      ),
+    },
+    {
+      path: "users",
+      element: <Users />
+    },
+    {
+      path: "products",
+      element: <Products />
+    },
+  ]);
+
   return (
-    <div><Home/></div>
+   <RouterProvider router={router}/>
   )
 }
 
