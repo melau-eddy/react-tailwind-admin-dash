d614f0851117951ba3ead1a6bd04e82a6c4f28ba
melau-eddy <eddymutugik@gmail.com>
2025-03-11 04:35:12 +0300

    App.tsx cpn updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,54 @@ import Home from "./pages/home/Home"
 import {
   createBrowserRouter,
   RouterProvider,
- // Route,
-  //Link,
+  Outlet
 } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
 
 function App() {
+
+
+  const Layout = ()=>(
+    return (
+      <div className="main">
+          <Navbar />
+            <div className="container">
+              <div className="menuContainer">
+                <Menu />
+              </div>
+              <div className="contentContainer">
+                 <Outlet /> 
+              </div>
+            </div>
+          <Footer />
+      </div>
+    )
+  )
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <Home/>
-      ),
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ]
     },
-    {
-      path: "users",
-      element: <Users />
-    },
-    {
-      path: "products",
-      element: <Products />
-    },
+
   ]);
 
   return (
