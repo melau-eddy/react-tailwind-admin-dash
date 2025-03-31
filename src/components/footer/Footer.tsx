fd95afad4ab4d77b35bc7d0d30a477b011e297d6
melau-eddy <eddymutugik@gmail.com>
2025-03-31 19:32:06 +0300

    navigation bar tsx span

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div className="footer">footer</div>
+    <div className="footer">
+      <span>eddyadmin</span>
+      <span>Melau Eddy Admin Dashboard</span>
+    </div>
   )
 }
 
