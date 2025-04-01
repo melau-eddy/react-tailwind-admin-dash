27398a85a2edf8c7c68fe2214523c53eb3bb28d9
melau-eddy <eddymutugik@gmail.com>
2025-04-01 19:35:47 +0300

    Responsive container import

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,5 +1,53 @@
-import { Link } from "react-router-dom"
-import "./chartBox.scss"
+import { Link } from "react-router-dom";
+import "./chartBox.scss";
+import { Line, LineChart, ResponsiveContainer } from "recharts";
+
+
+const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  
 
 const ChartBox = () => {
   return (
@@ -14,7 +62,24 @@ const ChartBox = () => {
             View all
             </Link>
         </div>
-        <div className="chartInfo"></div>
+        <div className="chartInfo">
+            <div className="chart">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart width={300} height={100} data={data}>
+                   <Line 
+                   type="monotone" 
+                   dataKey="pv" 
+                   stroke="#8884d8"
+                    strokeWidth={2} 
+                    />
+              </LineChart>
+            </ResponsiveContainer>
+            </div>
+            <div className="texts">
+                <span className="percentage">45%</span>
+                <span className="duration">this month</span>
+            </div>
+        </div>
     </div>
   )
 }
