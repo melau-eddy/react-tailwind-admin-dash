ad31583eb7df1c1f064d6e525adf33cd7ae6fc85
melau-eddy <eddymutugik@gmail.com>
2025-04-01 19:23:54 +0300

    recharts link react-router-dom

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom"
 import "./chartBox.scss"
 
 const ChartBox = () => {
   return (
-    <div className="chartBox">ChartBox</div>
+    <div className="chartBox">
+        <div className="boxInfo">
+            <div className="title">
+                <img src="/user.svg" alt="" />
+                <span>Total Users</span>
+            </div>
+            <h1>11.238</h1>
+            <Link to="/">
+            View all
+            </Link>
+        </div>
+        <div className="chartInfo"></div>
+    </div>
   )
 }
 
