c6eed04bdaf63481d51afe59e82f893c0c7557ae
melau-eddy <eddymutugik@gmail.com>
2025-04-01 22:28:35 +0300

    PieChartBox added component

---
+++ src/components/pieChartBox/PieChartBox.tsx
@@ -0,0 +1,17 @@
+import "./pieChartBox.scss"
+const data = [
+    {name: "Mobile", value: 400, color: " #0088FE"},
+    {name: "Desktop", value: 300, color: " #00c49F"},
+    {name: "Laptop", value: 300, color: " #FFBB28"},
+    {name: "Tablet", value: 200, color: " #FF8042"},
+];
+
+const PieChartBox = () => {
+  return (
+    <div className="pieChartBox">
+
+    </div>
+  )
+}
+
+export default PieChartBox
