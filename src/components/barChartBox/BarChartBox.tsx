336459a350c6520bf1aae15e48b842d47dbeae9f
melau-eddy <eddymutugik@gmail.com>
2025-04-01 21:28:26 +0300

    header 1 props.Props insertion

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -1,8 +1,30 @@
 
+import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
+import "./barChartBox.scss";
 
-const BarChartBox = () => {
+type Props = {
+    title: string;
+    color: string;
+    dataKey: string;
+    chartData: object[];
+}
+
+const BarChartBox = (props:Props) => {
   return (
-    <div>BarChartBox</div>
+    <div className="barChartBox">
+        <h1>{props.title}</h1>
+        <div className="chart">
+        <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={150} height={40} data={props.chartData}>
+            <Tooltip
+            contentStyle={{background: " #2a3447", borderRadius: "5px"}}
+            labelStyle={{display: "none"}}
+            />
+          <Bar dataKey={props.dataKey} fill={props.color} />
+        </BarChart>
+      </ResponsiveContainer>
+        </div>
+    </div>
   )
 }
 
