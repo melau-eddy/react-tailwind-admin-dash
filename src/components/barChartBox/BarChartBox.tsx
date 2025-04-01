e1147b00856ab1e91e83a94fee5e61366f3651d4
melau-eddy <eddymutugik@gmail.com>
2025-04-01 22:18:21 +0300

    barChartBox flex

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -1,4 +1,3 @@
-
 import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
 import "./barChartBox.scss";
 
@@ -7,25 +6,26 @@ type Props = {
     color: string;
     dataKey: string;
     chartData: object[];
-}
+};
 
 const BarChartBox = (props:Props) => {
   return (
     <div className="barChartBox">
         <h1>{props.title}</h1>
         <div className="chart">
-        <ResponsiveContainer width="100%" height="100%">
-        <BarChart width={150} height={40} data={props.chartData}>
+        <ResponsiveContainer width="99%" height={150}>
+        <BarChart data={props.chartData}>
             <Tooltip
             contentStyle={{background: " #2a3447", borderRadius: "5px"}}
             labelStyle={{display: "none"}}
+            cursor={{fill: "none"}}
             />
           <Bar dataKey={props.dataKey} fill={props.color} />
         </BarChart>
       </ResponsiveContainer>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default BarChartBox
+export default BarChartBox;
